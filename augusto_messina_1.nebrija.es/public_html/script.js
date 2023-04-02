498cfdcea84befa2fe622655650cfddb41ffba2f
Augusto Messina <84212395+augusMessina@users.noreply.github.com>
2023-04-02 04:47:47 +0200

    add all

---
+++ augusto_messina_1.nebrija.es/public_html/script.js
@@ -0,0 +1,29 @@
+function validate() {
+
+    const inputUser = document.getElementById("username")
+    const inputPassword = document.getElementById("password")
+    const responseMSG = document.getElementById("responseMSG")
+
+    if(inputUser.value === "augusto1" && inputPassword.value === "my_password"){
+        responseMSG.innerHTML = "Authentication succesfull!"
+        responseMSG.style.color = "#77dd77"
+    } else {
+        responseMSG.innerHTML = "Invalid username or password"
+        responseMSG.style.color = "#ff6961"
+    }
+}
+
+function swapVisibility(){
+
+    const inputPassword = document.getElementById("password")
+    const eyeIcon = document.getElementById("eye-icon")
+
+    if(inputPassword.type === "password"){
+        inputPassword.type = "text"
+        eyeIcon.className = "gg-eye-alt"
+    } else {
+        inputPassword.type = "password"
+        eyeIcon.className = "gg-eye"
+    }
+
+}
